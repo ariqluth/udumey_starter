1c173dde840cc42d5fb1426b2873a38270ff48c1
ariqluth <ttzluthfi@gmail.com>
2023-01-18 19:50:22 +0700

    first commit

---
+++ app/Http/Controllers/Api/QuoteController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreQuoteRequest;
+use App\Http\Requests\UpdateQuoteRequest;
+use App\Http\Resources\QuoteResource;
+use App\Models\Quote;
+use Illuminate\Http\Request;
+
+class QuoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return QuoteResource::collection(Quote::paginate(5));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreQuoteRequest $request)
+    {
+        //
+        return new QuoteResource(Quote::create($request->validated()));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Quote  $quote
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Quote $quote)
+    {
+        //
+        return new QuoteResource($quote);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Quote  $quote
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateQuoteRequest $request, Quote $quote)
+    {
+        //
+        $quote->update($request->validated());
+        return new QuoteResource($quote);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Quote  $quote
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Quote $quote)
+    {
+        //
+        $quote->delete();
+        return response()->noContent();
+    }
+}
